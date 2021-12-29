28a936457cf4480e9977abcd67d897f9c988755c
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-12-29 20:11:48 +0300

    handling onChange of the dropdown menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
+import { Listbox } from '@headlessui/react'
+import { useState } from 'react'
 import Logo from '../../assets/nav-logo.svg'
 import NavLink from './NavLink'
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from 'react-icons/md'
+const languages = [
+    { id: 1, lang: 'English', abbr: 'en' },
+    { id: 2, lang: 'Turkish', abbr: 'tr' },
+    { id: 3, lang: 'Arabic', abbr: 'ar' },
+    { id: 4, lang: 'Kurdish', abbr: 'kr' },
+]
 
 const Navbar = () => {
+    const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
     return (
-        <nav className="fixed top-0 left-0 w-full flex items-center justify-between bg-slate-800 px-section py-8">
-            <a href="#" className='w-44 h-26'>
+        <nav className="fixed top-0 left-0 w-full flex items-center justify-between bg-slate-800 px-sides py-8 z-auto">
+            <a href='#' className='w-44 h-26'>
                 <img src={Logo} alt="CLLPOOL Logo" className='w-full h-full' />
             </a>
             <div className='flex items-center gap-x-12 px-2 py-4'>
@@ -14,13 +24,26 @@ const Navbar = () => {
                 <NavLink destination={'#'} content={'our client'} />
                 <NavLink destination={'#'} content={'contact us'} />
             </div>
-            <select name="languages" id="languages">
-                <option value="en">English</option>
-                <option value="tr">Turkish</option>
-                <option value="ar">Arabic</option>
-                <option value="ku">کوردی</option>
-            </select>
-        </nav>
+
+            <Listbox value={selectedLanguage} onChange={(event) => {
+                setSelectedLanguage(languages.find(item => item.abbr === event))
+            }} as='div' className='flex flex-col justify-between items-stretch gap-4 border-2 border-white p-4'>
+                {({ open }) => (
+                    <>
+                        <Listbox.Button className='flex justify-between items-center gap-2 text-white'><span>{selectedLanguage.lang}</span>
+                            {!open && (<span><MdOutlineKeyboardArrowDown /></span>)}
+                            {open && (<span><MdOutlineKeyboardArrowUp /></span>)}</Listbox.Button>
+                        {open && (
+                            <Listbox.Options static className='relative text-left'>
+                                {languages.map((item => (
+                                    <Listbox.Option key={item.id} value={item.abbr} className='text-white px-1 border-b'>{item.lang}</Listbox.Option>
+                                )))}
+                            </Listbox.Options>
+                        )}
+                    </>
+                )}
+            </Listbox>
+        </nav >
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'sans': 'Rubik, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji'
       },
       spacing: {
-        'section': '10%'
+        'sides': '10%'
       },
 
       transitionDuration: {
