1f92acebc21e6b5a4ee6303f4311d071b09de977
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-01-12 16:04:56 +0300

    created Project component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from './header/Header';
 export { default as AboutUs } from './aboutUs/AboutUs';
 export { default as ContactUs } from './contact/ContactUs';
 export { default as OurClients } from './ourClients/OurClients';
+export { default as Projects } from './projects/Projects';
