eb7b1dd1fbdeb9cf36470b5ffd18a79176005a43
Revan Sarbast Salih <revansarbast8@gmail.com>
2022-02-05 21:43:38 +0300

    put all the data in a json file Fixes #26

--- src/components/contact/VisitUs.jsx
+++ src/components/contact/VisitUs.jsx
@@ -1,33 +1,45 @@
-import ReactMapGL, { Marker } from 'react-map-gl';
-import { useState } from 'react'
-import { HiOutlineLocationMarker } from 'react-icons/hi'
+import ReactMapGL, { Marker } from "react-map-gl";
+import { useState } from "react";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import data from "../../data.json";
 
-const CLLPOOL_COORDINATES = {
-    latitude: 36.197438,
-    longitude: 44.038536,
-}
 const VisitUs = () => {
-    const [viewPort, setViewPort] = useState({
-        latitude: CLLPOOL_COORDINATES.latitude,
-        longitude: CLLPOOL_COORDINATES.longitude,
-        zoom: 15.5,
-        maxZoom: 16,
-        minZoom: 15,
-    });
-    return (
-        <>
-            <h2 className='text-6xl text-center py-4 mt-4' > Visit Us</h2 >
+  const [viewPort, setViewPort] = useState({
+    latitude: data.CLLPOOL_COORDINATES.latitude,
+    longitude: data.CLLPOOL_COORDINATES.longitude,
+    zoom: 15.5,
+    maxZoom: 16,
+    minZoom: 15,
+  });
+  return (
+    <>
+      <h2 className="text-6xl text-center py-4 mt-4"> Visit Us</h2>
 
-            {/* map component */}
-            <div className='w-full h-128'>
-                <ReactMapGL ReactMapGL {...viewPort} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY} onViewStateChange={setViewPort} mapStyle="mapbox://styles/mapbox/dark-v10" width={'100%'} height={'100%'} >
-                    <Marker latitude={CLLPOOL_COORDINATES.latitude} longitude={CLLPOOL_COORDINATES.longitude} offsetLeft={-45} offsetTop={-35} className='flex flex-col items-center'>
-                        <p className='text-sm text-sky-500'>CLLPOOL Company</p> <HiOutlineLocationMarker className='text-2xl text-sky-500' />
-                    </Marker>
-                </ReactMapGL>
-            </div>
-        </>
-    )
-}
+      {/* map component */}
+      <div className="w-full h-128">
+        <ReactMapGL
+          ReactMapGL
+          {...viewPort}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+          onViewStateChange={setViewPort}
+          mapStyle="mapbox://styles/mapbox/dark-v10"
+          width={"100%"}
+          height={"100%"}
+        >
+          <Marker
+            latitude={data.CLLPOOL_COORDINATES.latitude}
+            longitude={data.CLLPOOL_COORDINATES.longitude}
+            offsetLeft={-45}
+            offsetTop={-35}
+            className="flex flex-col items-center"
+          >
+            <p className="text-sm text-sky-500">CLLPOOL Company</p>{" "}
+            <HiOutlineLocationMarker className="text-2xl text-sky-500" />
+          </Marker>
+        </ReactMapGL>
+      </div>
+    </>
+  );
+};
 
-export default VisitUs
+export default VisitUs;
--- src/components/ourClients/OurClients.jsx
+++ src/components/ourClients/OurClients.jsx
@@ -1,37 +1,51 @@
-import EmpireLogo from '../../assets/empire-logo.svg'
-import SpotifyLogo from '../../assets/spotify-logo.svg'
-import ClientBrand from './ClientBrand';
+import EmpireLogo from "../../assets/empire-logo.svg";
+import SpotifyLogo from "../../assets/spotify-logo.svg";
+import ClientBrand from "./ClientBrand";
 
 const clients = [
-    {
-        id: 1, logo: EmpireLogo, alt: 'logo'
-    },
-    {
-        id: 2, logo: SpotifyLogo, alt: 'logo'
-    },
-    {
-        id: 3, logo: EmpireLogo, alt: 'logo'
-    },
-    {
-        id: 4, logo: EmpireLogo, alt: 'logo'
-    },
-    {
-        id: 5, logo: EmpireLogo, alt: 'logo'
-    },
-    {
-        id: 6, logo: EmpireLogo, alt: 'logo'
-    },
-]
+  {
+    id: 1,
+    logo: EmpireLogo,
+    alt: "logo",
+  },
+  {
+    id: 2,
+    logo: SpotifyLogo,
+    alt: "logo",
+  },
+  {
+    id: 3,
+    logo: EmpireLogo,
+    alt: "logo",
+  },
+  {
+    id: 4,
+    logo: EmpireLogo,
+    alt: "logo",
+  },
+  {
+    id: 5,
+    logo: EmpireLogo,
+    alt: "logo",
+  },
+  {
+    id: 6,
+    logo: EmpireLogo,
+    alt: "logo",
+  },
+];
 
 const OurClients = () => {
-    return (
-        <section className="bg-slate-700 text-white px-sides py-8" id='clients'>
-            <h2 className="text-6xl capitalize text-center my-16">Our Clients</h2>
-            <div className='grid grid-cols-3 justify-items-center gap-x-4 gap-y-8 my-8'>
-                {clients.map(client => (<ClientBrand key={client.id} logo={client.logo} alt={client.alt} />))}
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="bg-slate-700 text-white px-sides py-8" id="clients">
+      <h2 className="text-6xl capitalize text-center my-16">Our Clients</h2>
+      <div className="grid grid-cols-3 justify-items-center gap-x-4 gap-y-8 my-8">
+        {clients.map((client) => (
+          <ClientBrand key={client.id} logo={client.logo} alt={client.alt} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default OurClients
+export default OurClients;
