4300351f674b01984121704d1ab3a285cdc8b732
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-02-09 21:12:54 +0300

    configure react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Navbar, Header, AboutUs, ContactUs, Projects, OurClients, Footer } from "./components"
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { Routes, Route, Link } from "react-router-dom"
 
 const App = () => {
   return (
