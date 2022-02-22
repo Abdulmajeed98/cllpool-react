73e68a2b7bf90d6c13fcf8413414b0faac4a1e78
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-02-22 15:50:54 +0300

    change: font family

--- src/components/global/SectionTitle.jsx
+++ src/components/global/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SectionTitle = ({ title }) => {
     return (
-        <h1 className="my-8 text-center text-5xl capitalize">{title}</h1>
+        <h1 className="my-8 text-center text-5xl capitalize font-medium">{title}</h1>
     )
 }
 
