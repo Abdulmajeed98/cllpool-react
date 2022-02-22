c82f23386f81b70a4d65b9cc0836e0f20d223e1b
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-02-22 22:16:06 +0300

    change: contact us <a> tag to HashLink Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import { lazy, Suspense } from 'react';
 import ArrowDown from '../../assets/icons/arrow-down.svg';
+import { HashLink } from 'react-router-hash-link';
 const HeaderVideo = lazy(() => import('./HeaderVideo'));
 const Header = () => {
     return (
         <header className="relative bg-black bg-opacity-50 w-screen h-screen flex flex-col justify-between items-center" id='home'>
-            <Suspense fallback={<div className='w-full h-full text-6xl text-red-600'>Loading</div>}>
+            <Suspense fallback={<div className='absolute inset-0 -z-10 w-full h-full text-6xl text-red-600'>Loading</div>}>
                 <HeaderVideo />
             </Suspense>
             <div className='flex flex-col flex-4 justify-center items-center gap-4 pt-8 w-full text-white'>
@@ -15,7 +16,7 @@ const Header = () => {
                 <h3 className='text-center text-lg'>
                     CLLPOOL: Pool, Sauna, Turkish Bath, Jacuzzi, Construction and Industry LTD.
                 </h3>
-                <a href='#contact-us' className='border-2 border-white px-8 py-4 mt-8 text-lg uppercase  hover:bg-sky-600 hover:border-sky-600 rounded-lg  hover:rounded-3xl transition-all duration-500'>Contact Us</a>
+                <HashLink to='/#contact-us' className='border-2 border-white px-8 py-4 mt-8 text-lg uppercase  hover:bg-sky-600 hover:border-sky-600 rounded-lg  hover:rounded-3xl transition-all duration-500'>Contact Us</HashLink>
             </div>
 
             <a href='#about' className='w-14 h-14 mb-8 animate-bounce'>
