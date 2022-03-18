7dbf0c1ae4666d821e6aa57a904562d5a3f4e8a4
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-03-18 19:37:29 +0300

    add: propTypes validation for ServiceCard

--- src/components/aboutUs/ServiceCard.jsx
+++ src/components/aboutUs/ServiceCard.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import PropTypes from "prop-types";
 const ServiceCard = ({ title, description, icon }) => {
   return (
     <div className="group h-72 w-11/12 overflow-hidden rounded-lg bg-sky-600 py-8 text-white">
@@ -8,10 +7,16 @@ const ServiceCard = ({ title, description, icon }) => {
           <div className="h-16 w-24 scale-150 transition-all  duration-300 group-hover:scale-125">{icon}</div>
           <h2 className="mt-6 text-3xl capitalize transition-all duration-300 group-hover:mt-2">{title}</h2>
         </div>
-        <p className="mt-8 w-2/3 translate-y-[200%] text-center text-xl transition-all  duration-300 group-hover:translate-y-0">{description}</p>
+        <p className="mt-8 w-2/3 translate-y-[200%] text-center text-xl transition-all duration-300 group-hover:translate-y-0">{description}</p>
       </div>
     </div>
   );
+};
+
+ServiceCard.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  icon: PropTypes.element,
 };
 
 export default ServiceCard;
