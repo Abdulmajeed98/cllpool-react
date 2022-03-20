a06c37126fbb701471e5342556803d1ed50d9d27
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-03-20 14:50:18 +0300

    add: prop-types to ClientBrand element

--- src/components/ourClients/ClientBrand.jsx
+++ src/components/ourClients/ClientBrand.jsx
@@ -1,9 +1,16 @@
+import PropTypes from "prop-types";
+
 const ClientBrand = ({ logo, alt }) => {
   return (
     <div className="group h-36 w-72">
       <img width={100} height={100} src={logo} alt={alt} className="h-full w-full object-fill group-hover:animate-pulse" />
     </div>
   );
+};
+
+ClientBrand.propTypes = {
+  logo: PropTypes.string,
+  alt: PropTypes.string,
 };
 
 export default ClientBrand;
